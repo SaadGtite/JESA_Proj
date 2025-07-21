26f6d26323fc53ed7784f3863344d7eeffff387f
fatimzahra203 <flifla.fatimzahra@gmail.com>
2025-07-21 12:12:56 +0100

    edit1

--- Front-end/src/Forms/EditProjectForm.js
+++ Front-end/src/Forms/EditProjectForm.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { Form, Button, Container, Alert } from 'react-bootstrap';
+import './EditProjectForm.css';
 
 const EditProjectForm = () => {
   const { id } = useParams();
